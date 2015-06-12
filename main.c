f7b8e82a9f141c3e9e235f923905ca999ff15368
Marko Djurovic <mdjurovic@deployinc.com>
2015-06-12 10:10:23 +0200

    Sceleton for 16f628a

--- main.c
+++ main.c
@@ -8,15 +8,16 @@
 
 #include "lcd.h"
 #include "speed.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 volatile unsigned char overflowCounter = 0;
 static unsigned int lastClock = 0;
 static unsigned char speed = 0;//first bit is previous value
 static char speedText[4];
-static float passedKms = 0.f;
+//static char passedKmsText[5];
+//static float passedKms = 0.f;
 
 void interrupt inter(void){
     if (T0IF){
@@ -37,14 +38,16 @@ int main(int argc, char** argv) {
     INTCON = 0b10100000;     
     speedText[2] = 'K';
     speedText[3] = 'm';
-    //speedText[4] = 'h';
+    //speedText[4] = 'h';    
+    //passedKmsText[3] = 'K';
+    //passedKmsText[4] = 'm';
         
     __delay_ms(40);
     
     InitLCD();
 //    TurnOff();
 //    TurnOn();
-    ClearLCDScreen();
+    //ClearLCDScreen();
     
     while (1){        
         if (PORTAbits.RA2){
@@ -56,21 +59,26 @@ int main(int argc, char** argv) {
                 TMR0 = 0x01;
                 T0IE = 1;
                 ClearLCDScreen();
-                PORTAbits.RA1 = 1;
+                PORTAbits.RA1 = 1;                
                 speed = caclculateSpeed((float)lastClock / 9765.625f); //hardcoded is 10M clock per second / 4 with prescaler 256;
                 //passedKms += (WHEEL_2RPI / 1000.f);
                 if (speed > 99)
                     speed = 99;                
-                char index = 2;
-                while (speed > 0){
-                    index--;
-                    speedText[index] = (speed % 10) + 0x30;
+                for (char index = 0; index < 2; index++){                    
+                    speedText[1 - index] = (speed % 10) + 0x30;                    
                     speed /= 10;
                 }                       
                 SetPosition(0x00, 0x06);
-                Write3StringToLcd(speedText, 4);                
-                speedText[0] = 0x30;
-                speedText[1] = 0x30;
+                Write3StringToLcd(speedText, 4);
+//                unsigned short passedKmsChar = (unsigned short)passedKms;                
+//                for (unsigned char index = 2; index >= 0; --index){                    
+//                    passedKmsText[index] = passedKmsChar - (passedKmsChar % 10) * 10 + 0x30;
+//                    passedKmsChar /= 10;
+//                }
+//                SetPosition(0x01, 0x05);
+//                Write3StringToLcd(passedKmsText, 5);
+//                EEPROM_WRITE(0xE5, speed);
+//                speed = EEPROM_READ(0xE5);
             }
             speed |= 0x80; // set previous value to 1;
         }
